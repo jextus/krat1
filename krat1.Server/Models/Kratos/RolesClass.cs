937b8e815cc030164080a9b91cad5c622da41fe9
johan <johan@DESKTOP-2JRC9UT>
2025-05-20 09:56:08 -0500

    Agregar archivos de proyecto.

---
+++ krat1.Server/Models/Kratos/RolesClass.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Krat1.Server.Models.Kratos
+{
+    public class RolesClass
+    {
+        public int id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres")]
+        public string nombre { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(500, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres")]
+        public string descripcion { get; set; }
+    }
+  
+}
---
+++ krat1.Server/Models/KratosContextcs.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using krat1.Server.Models.Kratos;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Practica.Server.Models
+{
+    public class KratosContext : DbContext
+    {
+        public KratosContext(DbContextOptions<KratosContext> options) : base(options)
+        {
+        }
+        public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Estado> Estado { get; set; }
+        public DbSet<Presentacion> Presentacion { get; set; }
+        public DbSet<Medicamento> Medicamento { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Categoria>()
+                .HasOne(c => c.EstadoFk)
+                .WithMany()
+                .HasForeignKey(c => c.estadoCategoria)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Presentacion>()
+                .HasOne(p => p.EstadoFk)
+                .WithMany()
+                .HasForeignKey(p => p.estadoPresentacion)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Medicamento>()
+                .HasOne(m => m.CategoriaFk)
+                .WithMany()
+                .HasForeignKey(m => m.categoriaId)
+                .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<Medicamento>()
+                .HasOne(m => m.PresentacionfK)
+                .WithMany()
+                .HasForeignKey(m => m.presentacionId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
