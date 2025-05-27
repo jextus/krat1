e092a304db73c9dead55fea5fc6947a925ac061d
MSantiagoVRamirez <m_santiago_r_99@outlook.com>
2025-05-27 14:03:00 -0500

    Service

--- krat1.Server/Services/Seguridad/Encriptar.cs
+++ krat1.Server/Services/Seguridad/Encriptar.cs
@@ -1,73 +1,22 @@
-﻿using krat1.Server.Services.Seguridad;
-using System.Security.Cryptography;
+﻿using System.Security.Cryptography;
 using System.Text;
 
-
-public class EncriptacionService : IEncriptarService
+namespace krat1.Server.Services.Seguridad;
+public class Encriptar
 {
-    private readonly byte[] _key;
-    private readonly byte[] _iv;
-
-    public EncriptacionService()
-    {
-        
-        _key = Encoding.UTF8.GetBytes("1234567890123456"); 
-        _iv = Encoding.UTF8.GetBytes("6543210987654321"); 
-    }
-
-    public string Encriptar(string textoPlano)
-    {
-        if (string.IsNullOrEmpty(textoPlano))
-            return string.Empty;
-
-        using var aes = Aes.Create();
-        aes.Key = _key;
-        aes.IV = _iv;
-
-        var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
-
-        using var ms = new MemoryStream();
-        using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
-        using (var sw = new StreamWriter(cs))
+        public static string EncriptarClave(string password)
         {
-            sw.Write(textoPlano);
+            StringBuilder sb = new StringBuilder();
+
+            using (SHA256 hash = SHA256.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                byte[] result = hash.ComputeHash(enc.GetBytes(password));
+
+                foreach (byte b in result)
+                    sb.Append(b.ToString("x2"));
+            }
+
+            return sb.ToString();
         }
-        return Convert.ToBase64String(ms.ToArray());
-    }
-
-    public bool VerificarHash(string textoPlano, string hash)
-    {
-        if (string.IsNullOrEmpty(textoPlano) || string.IsNullOrEmpty(hash))
-            return false;
-
-        try
-        {
-            string textoDesencriptado = Desencriptar(hash);
-            return textoDesencriptado == textoPlano;
-        }
-        catch
-        {
-         
-            return false;
-        }
-    }
-
-    public string Desencriptar(string textoCifrado)
-    {
-        if (string.IsNullOrEmpty(textoCifrado))
-            return string.Empty;
-
-        using var aes = Aes.Create();
-        aes.Key = _key;
-        aes.IV = _iv;
-
-        var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
-
-        var buffer = Convert.FromBase64String(textoCifrado);
-
-        using var ms = new MemoryStream(buffer);
-        using var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read);
-        using var sr = new StreamReader(cs);
-        return sr.ReadToEnd();
-    }
 }
--- krat1.Server/Services/Seguridad/IEncriptarService.cs
+++
@@ -1,7 +0,0 @@
-namespace krat1.Server.Services.Seguridad;
-public interface IEncriptarService
-{
-    string Encriptar(string textoPlano);
-    string Desencriptar(string textoCifrado);
-    bool VerificarHash(string textoPlano, string hash);
-}
