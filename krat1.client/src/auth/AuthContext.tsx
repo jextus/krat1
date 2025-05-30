13a7f3e40c3cfb2e8a73256d36efbca9809e5a10
jextus <129642498+jextus@users.noreply.github.com>
2025-05-30 13:54:20 -0500

    Update and rename AuthContext.jsx to AuthContext.tsx

---
+++ krat1.client/src/auth/AuthContext.tsx
@@ -0,0 +1,32 @@
+// src/auth/AuthContext.jsx
+import { createContext, useContext, UseState, ReactNode } from 'react';
+
+interface AuthContextType{
+  user: any;
+  login: (userData: any) => void;
+  logout: () => void;
+}
+  cont AuthContext = createContext<AuthContextType | null>(null);
+
+  export const AuthProvider = ( {children } : { children: ReactNode}) =>{
+    const [user, setUser] = useState<any>(null);
+
+    const login = (userData: any) => {
+      setUser(userData);
+      localStorage.setItem('user', JSON.strinify(userData));
+    };
+
+
+    const logout = () => {
+      setUser(null);
+      localStorage.removeItem('user');
+    };
+
+    return (
+      <AuthContext.Provider value={{user, login, logout}}>
+        {children}
+      </AuthContext.Provider>
+      );
+  };
+
+export const useAuth = () => useContext(AuthContext) as AuthContextType;
